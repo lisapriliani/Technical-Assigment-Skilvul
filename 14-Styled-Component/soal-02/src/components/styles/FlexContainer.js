baa5d32c8d68506cb5d325ae022aefee336c5570
lisapriliani <lisaapriliani4@gmail.com>
2021-11-04 10:23:00 +0700

    done

--- 14-Styled-Component/soal-02/src/components/styles/FlexContainer.js
+++ 14-Styled-Component/soal-02/src/components/styles/FlexContainer.js
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
 const FlexContainer = styled.div`
+  flex-wrap: wrap;
   height: 100vh;
   display: flex;
-  flex-wrap: wrap;
   flex-direction: ${(props) => props.direction || "row"};
   justify-content: ${(props) => props.justify || "center"};
   align-items: ${(props) => props.align || "center"};
